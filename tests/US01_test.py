f4bccb11133bb46b4b5e3c523ad1b818c6152f59
Daniel <dalj20@student.bth.se>
2023-04-06 08:44:14 +0200

    Initial commit of tests

---
+++ tests/US01_test.py
@@ -0,0 +1,15 @@
+import main_functions as mf
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+def test_increment():
+    ev3 = EV3Brick()
+    assert mf.increment(1) == 2
+
+
