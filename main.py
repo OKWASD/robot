e8c7edb8678085db656353a29d77d904e029393f
Daniel <dalj20@student.bth.se>
2023-04-12 13:52:53 +0200

    Sprint 1

--- main.py
+++ main.py
@@ -1,20 +1,72 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
+from pybricks.parameters import Port, Stop, Direction, Button
+from pybricks.tools import wait
 
+#Initialize EV3sensors and ev3brick
+robot = EV3Brick()
+grip = Motor(Port.A)
+arm = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
+base = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])
+base_touch = TouchSensor(Port.S1)
+color_sensor = ColorSensor(Port.S2)
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
+#Reset and calibrate arm and grip
+def calibrate():
+    #Zero arm
+    arm.run_time(-30, 1000)
+    arm.run(15)
+    while 0 < color_sensor.reflection() < 50:
+        wait(15)
+    arm.reset_angle(0)
+    arm.hold()
 
+    #Zero base
+    base.run(-60)
+    while not base_touch.pressed():
+        wait(15)
+    base.reset_angle(0)
+    base.hold()
 
-# Create your objects here.
-ev3 = EV3Brick()
+    #Zero grip
+    grip.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
+    grip.reset_angle(0)
+    grip.run_target(200, -90)
 
+#US01 && US03
+def pickup():
+    "Pickups object and checks if object exist"
+    arm.run_target(60, -40)
+    grip.run_until_stalled(200, then=Stop.HOLD, duty_limit=50)
+    if grip.angle() > -3:
+        grip.run_target(200, -90)
+    arm.run_target(60, 0)
 
-# Write your program here.
-ev3.speaker.beep()
+#US02
+def drop():
+    "Drops held object"
+    arm.run_target(60, -40)
+    grip.run_target(200, -90)
+    arm.run_target(60, 0)
+
+#US04
+def check_color() -> ():
+    "Checks color "
+    color = color_sensor.color()
+    robot.screen.print(color)
+    return color
+
+calibrate()
+
+while True:
+    pressedButtons = robot.buttons.pressed()
+    if(Button.DOWN in pressedButtons):
+        drop()
+        wait(100)
+    elif(Button.UP in pressedButtons):
+        pickup()
+        wait(100)
+    elif(Button.CENTER in pressedButtons):
+        check_color()
+        wait(100)
